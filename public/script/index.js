14bd2b105942d32adf9b02e2537b5103efa0e717
Ashok Kumar Yadav <ashokkumar.y@Ashok-261.local>
2020-04-23 12:32:38 +0530

    changes for timeline

--- public/script/index.js
+++ public/script/index.js
@@ -1,33 +1,45 @@
-document.onRea
 const getCommitButton = document.getElementById('get-commit');
 
 const commitResponse = document.getElementById('commit-result');
 
-getCommitButton.addEventListener('click',getCommitDetail);
+getCommitButton.addEventListener('click', getCommitDetail);
 
 
- async function getCommitDetail(){
-  const url = 'https://api.github.com/search/commits?q=repo:freecodecamp/freecodecamp author-date:2020-03-01..2020-04-01 ';
-  const headers = {
-      "Accept":"application/vnd.github.cloak-preview"
-  }
+async function getCommitDetail() {
+    commitResponse.textContent = "";
+    const url = 'https://api.github.com/search/commits?q=repo:freeCodeCamp/freeCodeCamp author-date:2020-03-01..2020-04-11';
+    const headers = {
+        "Accept": "application/vnd.github.cloak-preview"
+    }
 
-  const response =  await fetch(url,{
-      "headers":headers
-  });
+    const response = await fetch(url, {
+        "headers": headers
+    });
+  console.log("response",response);
+    const result = await response.json();
+        
+    result.items.forEach(element => {
+        const elementLI = document.createElement('li');
+        const elementDiv = document.createElement('div');
 
-  const result =  await response.json();
+        const authorName = document.createElement('div');
+        const dateCommit= document.createElement('div');
+        const msgCommit = document.createElement('div');
 
-  result.items.forEach(element => {
-      const elementDiv = document.createElement('div');
-      const dateSpan = document.createElement('span');
-      const nameSpan = document.createElement('span');
-      dateSpan.textContent = element.commit.author.date;
-      nameSpan.textContent = element.commit.author.name;
-      elementDiv.appendChild(nameSpan);
-      elementDiv.appendChild(dateSpan);
-      commitResponse.appendChild(elementDiv);
-  });
+        elementDiv.className = "list-item";
+        authorName.textContent = element.commit.author.date;
+        dateCommit.textContent = element.commit.author.name;
+        msgCommit.textContent = element.commit.message;
+        
+        elementDiv.appendChild(authorName);
+        elementDiv.appendChild(dateCommit);
+        elementDiv.appendChild(msgCommit);
 
-  console.log('result',result);
+        elementLI.appendChild(elementDiv);
+        commitResponse.appendChild(elementLI);
+    });
+    const elementDiv = document.createElement('div');
+    elementDiv.style.clear = 'both';
+    commitResponse.appendChild(elementDiv);
+    console.log('result', result);
 }
