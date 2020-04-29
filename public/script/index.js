7ae6b5f657e394287729f147f0951addacc5a9cb
Ashok Kumar Yadav <ashokkumar.y@Ashok-261.local>
2020-04-29 23:23:23 +0530

    adding input box

--- public/script/index.js
+++ public/script/index.js
@@ -5,41 +5,50 @@ const commitResponse = document.getElementById('commit-result');
 getCommitButton.addEventListener('click', getCommitDetail);
 
 
-async function getCommitDetail() {
-    commitResponse.textContent = "";
-    const url = 'https://api.github.com/search/commits?q=repo:freeCodeCamp/freeCodeCamp author-date:2020-03-01..2020-04-11';
-    const headers = {
-        "Accept": "application/vnd.github.cloak-preview"
+
+
+    async function getCommitDetail() {
+        const userName = document.getElementById("git-user").value;
+        const repoName = document.getElementById("git-repo").value;
+        if( userName !==  "" && repoName !== ""){
+        commitResponse.textContent = "";
+        document.getElementById("response").style.display = "block";
+        const url = `https://api.github.com/search/commits?q=repo:${userName.trim()}/${repoName.trim()} author-date:2020-03-01..2020-04-11`;
+        const headers = {
+            "Accept": "application/vnd.github.cloak-preview"
+        }
+    
+        const response = await fetch(url, {
+            "headers": headers
+        });
+        const result = await response.json();
+            
+        result.items.forEach(element => {
+            const elementLI = document.createElement('li');
+            const elementDiv = document.createElement('div');
+    
+            const authorName = document.createElement('div');
+            const dateCommit= document.createElement('div');
+            const msgCommit = document.createElement('div');
+    
+            elementDiv.className = "list-item";
+            authorName.textContent = element.commit.author.date;
+            dateCommit.textContent = element.commit.author.name;
+            msgCommit.textContent = element.commit.message;
+            
+            elementDiv.appendChild(authorName);
+            elementDiv.appendChild(dateCommit);
+            elementDiv.appendChild(msgCommit);
+    
+            elementLI.appendChild(elementDiv);
+            commitResponse.appendChild(elementLI);
+        });
+        const elementDiv = document.createElement('div');
+        elementDiv.style.clear = 'both';
+        commitResponse.appendChild(elementDiv);
     }
 
-    const response = await fetch(url, {
-        "headers": headers
-    });
-  console.log("response",response);
-    const result = await response.json();
-        
-    result.items.forEach(element => {
-        const elementLI = document.createElement('li');
-        const elementDiv = document.createElement('div');
-
-        const authorName = document.createElement('div');
-        const dateCommit= document.createElement('div');
-        const msgCommit = document.createElement('div');
-
-        elementDiv.className = "list-item";
-        authorName.textContent = element.commit.author.date;
-        dateCommit.textContent = element.commit.author.name;
-        msgCommit.textContent = element.commit.message;
-        
-        elementDiv.appendChild(authorName);
-        elementDiv.appendChild(dateCommit);
-        elementDiv.appendChild(msgCommit);
-
-        elementLI.appendChild(elementDiv);
-        commitResponse.appendChild(elementLI);
-    });
-    const elementDiv = document.createElement('div');
-    elementDiv.style.clear = 'both';
-    commitResponse.appendChild(elementDiv);
-    console.log('result', result);
+else{
+    alert("All inputs are required");
+}
 }
